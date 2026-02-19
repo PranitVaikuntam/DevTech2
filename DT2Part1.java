ace5cafe462af1629bdd955220bb0b2ae4d42f06
azhu48 <azhu48@wisc.edu>
2026-02-19 17:55:23 -0600

    added part1.java

---
+++ DT2Part1.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+class DevTech2Part1{
+	public static void main(String args[]){
+		try{
+			Connection dbCxn=DriveMAnager.getConnection(
+				"jdbc:mysql://localhost:53326/BoatsAndSailors","root","password"
+			);
+			
+			String boatQuery = "select * from Boat";
+			System.out.println(boatQuery);
+			Statement selectFromBoat=dbCxn.createStatement();
+			ResultSet rsBoat=selectFromBoat.executeQuery(boatQuery);
+			while(rsBoat.next()) {
+				System.out.println(rsBoat.getInt(1)		//Boat_pk
+					+ "  " + rsBoat.getString(2) 		//Boat_model
+					+ "  " + rsBoat.getString(3)		//Boat_name
+					+ "  " + rsBoat.getFloat(4)			//Boat_LOA
+				);
+			}
+			dbCxn.close();
+		}catch(Exception e){ System.out.println(e);}
+	}
+}
