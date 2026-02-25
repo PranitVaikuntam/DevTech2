272111a6f7956643353c509d2ba91aaa172b8dd8
Pranit Vaikuntam <vaikuntam@wisc.edu>
2026-02-25 17:29:32 -0600

    Partially Done

--- DT2Part1.java
+++ DT2Part1.java
@@ -2,7 +2,7 @@ import java.sql.*;
 class DevTech2Part1{
 	public static void main(String args[]){
 		try{
-			Connection dbCxn=DriveMAnager.getConnection(
+			Connection dbCxn=DriverManager.getConnection(
 				"jdbc:mysql://localhost:53326/BoatsAndSailors","root","password"
 			);
 			
---
+++ Sailor.sql
@@ -0,0 +1,8 @@
+mysql> CREATE TABLE `BoatsAndSailors`.`Sailor` (
+    `Sailor_pk` INT NOT NULL auto_increment,
+    `Sailor_name` VARCHAR(50) NULL,
+    `Sailor_role` ENUM('skipper', 'crew'),
+    `Sailor_DOB` DATETIME,
+    `SailorBoat_pk` INT NULL,
+    PRIMARY KEY (`Sailor_pk`)
+);
